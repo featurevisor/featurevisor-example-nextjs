db812e4df2d1a735f14419a4601e6c6465306607
Fahad Heylaal <fahad19@gmail.com>
2025-07-21 01:16:13 +0200

    align with guide on website

---
+++ src/featurevisor.ts
@@ -0,0 +1,46 @@
+import { createInstance } from "@featurevisor/sdk";
+
+/**
+ * Constants
+ */
+const DATAFILE_URL =
+  "https://featurevisor-example-cloudflare.pages.dev/production/featurevisor-tag-all.json";
+const REFRESH_INTERVAL = 5 * 60 * 1000; // 5 minutes
+
+/**
+ * Featurevisor specific functions
+ */
+export const f = createInstance({});
+
+let initialFetchCompleted = false;
+
+export function fetchAndSetDatafile() {
+  console.log("[Featurevisor] Fetching datafile from:", DATAFILE_URL);
+
+  const result = fetch(DATAFILE_URL)
+    .then((response) => response.json())
+    .then((datafile) => f.setDatafile(datafile))
+    .catch((error) =>
+      console.error("[Featurevisor] Error fetching datafile:", error)
+    );
+
+  initialFetchCompleted = true;
+
+  return result;
+}
+
+export async function getInstance() {
+  if (initialFetchCompleted) {
+    return f;
+  }
+
+  await fetchAndSetDatafile();
+
+  return f;
+}
+
+if (REFRESH_INTERVAL > 0) {
+  setInterval(async () => {
+    await fetchAndSetDatafile();
+  }, REFRESH_INTERVAL);
+}
