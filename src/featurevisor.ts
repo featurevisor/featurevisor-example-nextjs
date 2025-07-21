08cec39ef3f6e6ce05da6131bb33db8455a1a724
Fahad Heylaal <fahad19@gmail.com>
2025-07-21 17:45:07 +0200

    Featurevisor adapter for Flags SDK (#4)

--- src/featurevisor.ts
+++ src/featurevisor.ts
@@ -1,46 +1,81 @@
-import { createInstance } from "@featurevisor/sdk";
+import type { Adapter } from "flags";
+import { createInstance, FeaturevisorInstance } from "@featurevisor/sdk";
 
-/**
- * Constants
- */
-const DATAFILE_URL =
-  "https://featurevisor-example-cloudflare.pages.dev/production/featurevisor-tag-all.json";
-const REFRESH_INTERVAL = 5 * 60 * 1000; // 5 minutes
-
-/**
- * Featurevisor specific functions
- */
-export const f = createInstance({});
-
-let initialFetchCompleted = false;
-
-export function fetchAndSetDatafile() {
-  console.log("[Featurevisor] Fetching datafile from:", DATAFILE_URL);
-
-  const result = fetch(DATAFILE_URL)
-    .then((response) => response.json())
-    .then((datafile) => f.setDatafile(datafile))
-    .catch((error) =>
-      console.error("[Featurevisor] Error fetching datafile:", error)
-    );
-
-  initialFetchCompleted = true;
-
-  return result;
+export interface FeaturevisorAdapterOptions {
+  datafileUrl: string;
+  refreshInterval?: number;
+  f?: FeaturevisorInstance;
 }
 
-export async function getInstance() {
-  if (initialFetchCompleted) {
-    return f;
+export interface FeaturevisorEntitiesType {
+  userId?: string;
+  // ...more properties for your context can be added here
+}
+
+export function createFeaturevisorAdapter(options: FeaturevisorAdapterOptions) {
+  const f = options.f || createInstance({});
+  let initialFetchCompleted = false;
+
+  // datafile fetcher
+  function fetchAndSetDatafile() {
+    console.log("[Featurevisor] Fetching datafile from:", options.datafileUrl);
+
+    const result = fetch(options.datafileUrl)
+      .then((response) => response.json())
+      .then((datafile) => {
+        f.setDatafile(datafile);
+        initialFetchCompleted = true;
+      })
+      .catch((error) =>
+        console.error("[Featurevisor] Error fetching datafile:", error)
+      );
+
+    return result;
   }
 
-  await fetchAndSetDatafile();
+  // datafile refresher (periodic update)
+  if (options.refreshInterval) {
+    setInterval(async () => {
+      await fetchAndSetDatafile();
+    }, options.refreshInterval);
+  }
 
-  return f;
-}
+  // adapter
+  return function featurevisorAdapter<
+    ValueType,
+    EntitiesType extends FeaturevisorEntitiesType
+  >(): Adapter<ValueType, EntitiesType> {
+    return {
+      async decide({ key, entities, headers, cookies }): Promise<ValueType> {
+        // ensure the datafile is fetched before making decisions
+        if (!initialFetchCompleted) {
+          await fetchAndSetDatafile();
+        }
 
-if (REFRESH_INTERVAL > 0) {
-  setInterval(async () => {
-    await fetchAndSetDatafile();
-  }, REFRESH_INTERVAL);
+        const context = {
+          userId: entities?.userId,
+        };
+
+        // mapping passed key to Featurevisor SDK methods:
+        //
+        //   - "<featureKey>"               => f.isEnabled(key, context)
+        //   - "<featureKey>:variation"     => f.getVariation(key, context)
+        //   - "<featureKey>:<variableKey>" => f.getVariable(key, variableKey, context)
+        const [featureKey, variableKey] = key.split(":");
+
+        if (variableKey) {
+          if (variableKey === "variation") {
+            // variation
+            return f.getVariation(featureKey, context) as ValueType;
+          } else {
+            // variable
+            return f.getVariable(featureKey, variableKey, context) as ValueType;
+          }
+        }
+
+        // flag
+        return f.isEnabled(featureKey, context) as ValueType;
+      },
+    };
+  };
 }
